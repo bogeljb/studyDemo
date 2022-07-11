f2b399522b83111016c0c9bec289f2070e12be37
bogeljb <1269098020@qq.com>
2022-07-11 21:51:58 +0800

    修改后的main.js

--- main.js
+++ main.js
@@ -8,3 +8,20 @@ function fn(a,b) {
 }
 
 console.log(fn(1))
+let cp  ={
+    0:"路飞",
+    1:"女帝"
+}
+var str = "";
+for(var i in cp) {
+    str += i+':'+cp[i]+'\n'
+}
+console.log(str)
+
+
+function fn(a,b) {
+    return a+b;
+}
+
+console.log(fn(1));
+
